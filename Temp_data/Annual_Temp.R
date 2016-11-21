28821316fe3f0d981421343774001b304c4f9cef
Adelingtan <adelingtan>
2016-11-20 18:10:58 -0800

    update plot

--- Temp_data/Annual_Temp.R
+++ Temp_data/Annual_Temp.R
@@ -1,5 +1,6 @@
 library(ggplot2)
-
+install.packages("viridis") 
+library(viridis)
 temp<-read.csv("temp_by_month.csv")
 temp<-temp[1:1577,]
 colnames(temp)<-c("year","month","avg_temp")
@@ -18,8 +19,7 @@ temp_by_season<-new_data[order(new_data$Group.1),]
 colnames(temp_by_season)<-c("year","season","avg_temp")
 
 
-
-#####Create the graph#####
+#####Create the line graph#####
 gg<-ggplot(temp_by_season, aes(x=year, y=avg_temp,colour =season))+
   geom_point(alpha = 0.7,size = 3)+
   geom_line(data=temp_year, aes(x=year, y=avg_temp),color='red',size=1.3)+
@@ -31,3 +31,20 @@ gg<-ggplot(temp_by_season, aes(x=year, y=avg_temp,colour =season))+
   theme(legend.position = c(.1, 0.75))
 gg
 
+#####Create Heatmaps#####
+new_temp<-temp[1200:1571,]
+Month<-c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+Month <- factor(Month, levels=unique(Month))
+monthly<-data.frame(new_temp$year,Month,new_temp$avg_temp)
+colnames(monthly)<-c("Year","Month","avg_temp")
+
+gg <- ggplot(monthly, aes(x=Year, y=Month, fill=avg_temp))+
+      coord_equal()+
+      scale_fill_viridis(name="temperature")+
+      geom_tile(color="white", size=0.1)+
+     theme(legend.title=element_text(size=8))+
+     labs(title="Monthly average temperature 1980-2010\n")
+
+gg
+ggsave("Heatmaps.png")
+dev.off()
